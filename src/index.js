72afbc6f2f611006e0c4376c92336336800738f3
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-12 14:30:38 +0100

    add function to ensure only one dropDown is open at once

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import './style.css';
 
 const clickDropDownBtns = document.querySelectorAll('.click-drop-down-btn');
 
+function closeOpenDropContent() {
+    const dropDownContents = document.querySelectorAll('.click-drop-down-content');
+
+    dropDownContents.forEach((dropContent) => {
+        dropContent.classList.remove('clicked');
+    })
+}
+
 function openDropContent(e) {
   const elm = e.currentTarget;
   const target = elm.dataset.targetDropContent;
 
+  closeOpenDropContent();
+
   const dropDownContent = document.querySelector(`#${target}`);
-  dropDownContent.classList.toggle('clicked');
+
+  if (dropDownContent === null) return;
+  dropDownContent.classList.add('clicked');
 }
+
+
 
 clickDropDownBtns.forEach((btn) => {
   btn.addEventListener('click', openDropContent);
