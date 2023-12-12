9c504d388aaeaa3854fe91d2c8e30e481ed2c2c5
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-12 15:20:42 +0100

    add state to ensure closing other open dropContent

--- src/dropDown.js
+++ src/dropDown.js
@@ -1,35 +1,47 @@
+let DROPCONTENTSELECTOR = "";
+
+function closeOpenDropContent(dropContentSelector, triggerClass) {
+    const dropDownContents = document.querySelectorAll(dropContentSelector);
+
+    dropDownContents.forEach((dropContent) => {
+      if (dropContent !== null) {
+        dropContent.classList.remove(triggerClass);
+      }
+    });
+  }
+
 function openDropContent(dropContentName, triggerClass) {
   const dropContent = document.querySelector(`#${dropContentName}`);
 
+  closeOpenDropContent(DROPCONTENTSELECTOR, triggerClass);
+
   if (dropContent === null) {
-    throw new Error(`Failed to assigned ${triggerClass} to a null`)
+    throw new Error(`Failed to assigned ${triggerClass} to a null`);
   } else {
-    dropContent.classList.add(triggerClass);
+    const { classList } = dropContent;
+    if (classList.contains(triggerClass)) {
+      dropContent.classList.remove(triggerClass);
+    } else {
+      dropContent.classList.add(triggerClass);
+    }
   }
 }
 
-function closeOpenDropContent(dropContentSelector, triggerClass) {
-  const dropDownContents = document.querySelectorAll(dropContentSelector);
 
-  dropDownContents.forEach((dropContent) => {
-    if (dropContent !== null) {
-      dropContent.classList.remove(triggerClass);
-    }
-  });
-}
 
 export default function initTriggerButton(
   triggerBtnSelector,
   triggerClass,
   dropContentSelector
 ) {
+    DROPCONTENTSELECTOR = dropContentSelector;
+
   const triggerButtons = document.querySelectorAll(triggerBtnSelector);
 
   triggerButtons.forEach((triggerBtn) => {
     triggerBtn.addEventListener('click', (e) => {
       const elm = e.currentTarget;
-      const {targetDropContent} = elm.dataset;
-      closeOpenDropContent(dropContentSelector, triggerClass);
+      const { targetDropContent } = elm.dataset;
       openDropContent(targetDropContent, triggerClass);
     });
   });
