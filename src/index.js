4c0df42a48431aec8b84a5c945aaa8f2a06074ca
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-12 15:08:19 +0100

    use dropdown module to operate dropdown showing and hiding

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 import './style.css';
+import initTriggerButton from './dropDown';
 
-const clickDropDownBtns = document.querySelectorAll('.click-drop-down-btn');
-
-function closeOpenDropContent() {
-    const dropDownContents = document.querySelectorAll('.click-drop-down-content');
-
-    dropDownContents.forEach((dropContent) => {
-        dropContent.classList.remove('clicked');
-    })
-}
-
-function openDropContent(e) {
-  const elm = e.currentTarget;
-  const target = elm.dataset.targetDropContent;
-
-  closeOpenDropContent();
-
-  const dropDownContent = document.querySelector(`#${target}`);
-
-  if (dropDownContent === null) return;
-  dropDownContent.classList.add('clicked');
-}
-
-
-
-clickDropDownBtns.forEach((btn) => {
-  btn.addEventListener('click', openDropContent);
-});
+initTriggerButton(
+  '[data-target-drop-content]',
+  'clicked',
+  '.click-drop-down-content'
+);
