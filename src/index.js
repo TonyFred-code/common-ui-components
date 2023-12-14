8adb5807b0c579d715bcb2757b24dea47aef2406
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-14 04:08:31 +0100

    import and use tab module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './style.css';
 import initTriggerButton from './dropDown';
+import initTabTriggers from './tab';
 
 initTriggerButton(
   '[data-target-drop-content]',
   'clicked',
   '.click-drop-down-content'
 );
+
+initTabTriggers('active', '.tab-link');
