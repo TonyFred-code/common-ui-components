cbeb5dbc63523b9fc31131b5d896aa7a9d75284b
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-12-16 02:04:01 +0100

    format file

--- src/tab.js
+++ src/tab.js
@@ -2,39 +2,36 @@ let TABTRIGGER = '';
 let TABSELECTOR = '';
 
 function setActive(e) {
-    const tabClicked = e.currentTarget;
+  const tabClicked = e.currentTarget;
 
-    tabClicked.classList.add(TABTRIGGER);
+  tabClicked.classList.add(TABTRIGGER);
 }
 
 function closeOpen() {
-    const tabLinks = document.querySelectorAll(TABSELECTOR);
+  const tabLinks = document.querySelectorAll(TABSELECTOR);
 
-    tabLinks.forEach((tabLink) => {
-        if (tabLink === null) {
-            throw new Error("Setting class attribute on null.");
-        } else {
-            tabLink.classList.remove(TABTRIGGER);
-        }
-    })
+  tabLinks.forEach((tabLink) => {
+    if (tabLink === null) {
+      throw new Error('Setting class attribute on null.');
+    } else {
+      tabLink.classList.remove(TABTRIGGER);
+    }
+  });
 }
 
-export default function initTabTriggers(
-    classTriggerName,
-    tabLinkSelector
-) {
-    const tabLinks = document.querySelectorAll(tabLinkSelector);
+export default function initTabTriggers(classTriggerName, tabLinkSelector) {
+  const tabLinks = document.querySelectorAll(tabLinkSelector);
 
-    tabLinks.forEach((tabLink) => {
-        if (tabLink === null) {
-            throw new Error("Setting eventlistener on null.");
-        } else {
-            TABTRIGGER = classTriggerName;
-            TABSELECTOR = tabLinkSelector;
-            tabLink.addEventListener("click", (e) => {
-                closeOpen();
-                setActive(e);
-            })
-        }
-    })
+  tabLinks.forEach((tabLink) => {
+    if (tabLink === null) {
+      throw new Error('Setting eventlistener on null.');
+    } else {
+      TABTRIGGER = classTriggerName;
+      TABSELECTOR = tabLinkSelector;
+      tabLink.addEventListener('click', (e) => {
+        closeOpen();
+        setActive(e);
+      });
+    }
+  });
 }
